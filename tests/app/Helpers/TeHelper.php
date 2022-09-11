afdb9ef5768a3eeb5c8d6cb43cbd9ab2990a65a8
Mubeen Khan <57445302+itsmemubeen@users.noreply.github.com>
2022-09-11 19:02:12 +0500

    Refactored

--- tests/app/Helpers/TeHelper.php
+++ tests/app/Helpers/TeHelper.php
@@ -14,16 +14,18 @@ class TeHelper
     public static function fetchLanguageFromJobId($id)
     {
         $language = Language::findOrFail($id);
-        return $language1 = $language->language;
+        return $language->language;
     }
 
     public static function getUsermeta($user_id, $key = false)
     {
-        return $user = UserMeta::where('user_id', $user_id)->first()->$key;
-        if (!$key)
-            return $user->usermeta()->get()->all();
+
+        if($user_id)
+            return UserMeta::where('user_id', $user_id)->first();
+        elseif (!$key)
+            return UserMeta::all();
         else {
-            $meta = $user->usermeta()->where('key', '=', $key)->get()->first();
+            $meta = UserMeta::where('key', '=', $key)->get()->first();
             if ($meta)
                 return $meta->value;
             else return '';
@@ -32,12 +34,7 @@ class TeHelper
 
     public static function convertJobIdsInObjs($jobs_ids)
     {
-
-        $jobs = array();
-        foreach ($jobs_ids as $job_obj) {
-            $jobs[] = Job::findOrFail($job_obj->id);
-        }
-        return $jobs;
+        return Job::whereIn('id', $jobs_ids)->get();
     }
 
     public static function willExpireAt($due_time, $created_at)
@@ -47,13 +44,12 @@ class TeHelper
 
         $difference = $due_time->diffInHours($created_at);
 
-
-        if($difference <= 90)
-            $time = $due_time;
-        elseif ($difference <= 24) {
+        if ($difference <= 24) {
             $time = $created_at->addMinutes(90);
-        } elseif ($difference > 24 && $difference <= 72) {
+        } elseif ($difference <= 72) {
             $time = $created_at->addHours(16);
+        } elseif ($difference <= 90) {
+            $time = $due_time;
         } else {
             $time = $due_time->subHours(48);
         }
@@ -63,4 +59,3 @@ class TeHelper
     }
 
 }
-
--- tests/app/Repository/UserRepository.php
+++ tests/app/Repository/UserRepository.php
@@ -43,29 +43,25 @@ class UserRepository extends BaseRepository
 
     public function createOrUpdate($id = null, $request)
     { 
-        $model = is_null($id) ? new User : User::findOrFail($id);
-        $model->user_type = $request['role'];
-        $model->name = $request['name'];
-        $model->company_id = $request['company_id'] != '' ? $request['company_id'] : 0;
-        $model->department_id = $request['department_id'] != '' ? $request['department_id'] : 0;
-        $model->email = $request['email'];
-        $model->dob_or_orgid = $request['dob_or_orgid'];
-        $model->phone = $request['phone'];
-        $model->mobile = $request['mobile'];
-
-
-        if (!$id || $id && $request['password']) $model->password = bcrypt($request['password']);
-        $model->detachAllRoles();
-        $model->save();
-        $model->attachRole($request['role']);
+       $validate_data = $this->validate($request->all());
+       $user = null;
+       if($id){
+           $user = User::find($id);
+           $user->update($validate_data);
+       }
+       else{
+           $user = User::create($validate_data);
+       }
+        if (!$id || $id && $request['password']) $user->password = bcrypt($request['password']);
+        $user->role->sync($request['role']);
         $data = array();
 
-        if ($request['role'] == env('CUSTOMER_ROLE_ID')) {
+        if ($request['role'] == env('CUSTOMER_ROLE_ID') && $request['consumer_type'] == 'paid' &&
+            $request['company_id'] == '')
+        {
 
-            if($request['consumer_type'] == 'paid')
+            if($request['consumer_type'] == 'paid' && $request['company_id'] == '')
             {
-                if($request['company_id'] == '')
-                {
                     $type = Type::where('code', 'paid')->first();
                     $company = Company::create(['name' => $request['name'], 'type_id' => $type->id, 'additional_info' => 'Created automatically for user ' . $model->id]);
                     $department = Department::create(['name' => $request['name'], 'company_id' => $company->id, 'additional_info' => 'Created automatically for user ' . $model->id]);
@@ -73,31 +69,11 @@ class UserRepository extends BaseRepository
                     $model->company_id = $company->id;
                     $model->department_id = $department->id;
                     $model->save();
-                }
             }
 
-            $user_meta = UserMeta::firstOrCreate(['user_id' => $model->id]);
-            $old_meta = $user_meta->toArray();
-            $user_meta->consumer_type = $request['consumer_type'];
-            $user_meta->customer_type = $request['customer_type'];
-            $user_meta->username = $request['username'];
-            $user_meta->post_code = $request['post_code'];
-            $user_meta->address = $request['address'];
-            $user_meta->city = $request['city'];
-            $user_meta->town = $request['town'];
-            $user_meta->country = $request['country'];
-            $user_meta->reference = (isset($request['reference']) && $request['reference'] == 'yes') ? '1' : '0';
-            $user_meta->additional_info = $request['additional_info'];
-            $user_meta->cost_place = isset($request['cost_place']) ? $request['cost_place'] : '';
-            $user_meta->fee = isset($request['fee']) ? $request['fee'] : '';
-            $user_meta->time_to_charge = isset($request['time_to_charge']) ? $request['time_to_charge'] : '';
-            $user_meta->time_to_pay = isset($request['time_to_pay']) ? $request['time_to_pay'] : '';
-            $user_meta->charge_ob = isset($request['charge_ob']) ? $request['charge_ob'] : '';
-            $user_meta->customer_id = isset($request['customer_id']) ? $request['customer_id'] : '';
-            $user_meta->charge_km = isset($request['charge_km']) ? $request['charge_km'] : '';
-            $user_meta->maximum_km = isset($request['maximum_km']) ? $request['maximum_km'] : '';
-            $user_meta->save();
-            $new_meta = $user_meta->toArray();
+            $meta_data = $this->validate($request->all(), $meta_rules);
+
+            $user_meta = UserMeta::firstOrCreate(['user_id' => $model->id], $meta_data);
 
             $blacklistUpdated = [];
             $userBlacklist = UsersBlacklist::where('user_id', $id)->get();
@@ -131,29 +107,9 @@ class UserRepository extends BaseRepository
 
         } else if ($request['role'] == env('TRANSLATOR_ROLE_ID')) {
 
-            $user_meta = UserMeta::firstOrCreate(['user_id' => $model->id]);
+            $meta_data = $this->validate($request->all(), $meta__translator_rules);
 
-            $user_meta->translator_type = $request['translator_type'];
-            $user_meta->worked_for = $request['worked_for'];
-            if ($request['worked_for'] == 'yes') {
-                $user_meta->organization_number = $request['organization_number'];
-            }
-            $user_meta->gender = $request['gender'];
-            $user_meta->translator_level = $request['translator_level'];
-            $user_meta->additional_info = $request['additional_info'];
-            $user_meta->post_code = $request['post_code'];
-            $user_meta->address = $request['address'];
-            $user_meta->address_2 = $request['address_2'];
-            $user_meta->town = $request['town'];
-            $user_meta->save();
-
-            $data['translator_type'] = $request['translator_type'];
-            $data['worked_for'] = $request['worked_for'];
-            if ($request['worked_for'] == 'yes') {
-                $data['organization_number'] = $request['organization_number'];
-            }
-            $data['gender'] = $request['gender'];
-            $data['translator_level'] = $request['translator_level'];
+            UserMeta::firstOrCreate(['user_id' => $user->id], $meta_data);
 
             $langidUpdated = [];
             if ($request['user_language']) {
